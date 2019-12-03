d0f79fdf1059a1351a857276ec724773f6ce2dfd
Dohoon <ddddkim0525@naver.com>
2019-12-03 15:14:24 +0900

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "squire-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://4mduz39mj0.execute-api.us-east-1.amazonaws.com/prod and the region is us-east-1"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_8jPFYs9Av",
+      APP_CLIENT_ID: "28280mevksju0eda02kmnsn64p",
+      IDENTITY_POOL_ID: "us-east-1:819bedd6-9c05-4d68-b360-e8ad989bb0a2"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Squire</h1>
+        <p>A Decenteralized Cloud Platform</p>
+      </div>
+    </div>
+  );
+}
